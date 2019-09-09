ca6ab988961d688f8fcb3be8180e2f6e946abf2f
ke_li <stokid@126.com>
2019-09-09 17:13:53 +0800

    添加license

--- sample/src/main.js
+++ sample/src/main.js
@@ -3,8 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+const RxVuex = require('rx-vuex')
 Vue.config.productionTip = false
-    /* eslint-disable no-new */
+Vue.use(RxVuex)
 new Vue({
     el: '#app',
     router,
---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+import {RxVuex} from './rxvuex'
+
+export default RxVuex;
